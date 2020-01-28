78dff592f0da633b736e7721165d350b34b6aabf
bishabosha <bishbashboshjt@gmail.com>
2020-01-28 01:18:18 +0100

    use new macro api to unseal lambdas and blocks

--- eec-core/src/main/scala/mesa/eec/TypeTraverser.scala
+++ eec-core/src/main/scala/mesa/eec/TypeTraverser.scala
@@ -8,28 +8,32 @@ object TypeTraverser with
 
   def \[T, U](f: T =>*: U): T =>*: U = f
 
-  case object !
-
-  type ![A] = (A & !.type) // remember to unwrap expressions of this type to prevent classcastexception
+  case class ![A](a: A)
 
   given BangOps: [A](x: A) extended with
-    def unary_! : ![A] = x.asInstanceOf[![A]]
+    def repeatable : ![A] = new !(x)
 
   def traverseTypesImpl[T: Type](expr: Expr[T])(given qctx: QuoteContext): Expr[Unit] =
-    import qctx.tasty.{Type => _, _,given}
+    import qctx.tasty.{Type => TastyType, _,given}
 
     expr match
       case '{ \($x: $t =>*: $u) } => traverseTypesImpl(x)
 
-      case f @ '{ type $t; type $u; ($x: `$t`) => ($body: `$u`) } =>
-        if f.unseal.tpe <:< typeOf[$t =>*: $u]
-          '{ println(s"\\(${${Expr(x.name)}}: ${${Expr(t.show)}}) =>* (${${Expr(body.show)}}): ${${Expr(u.show)}}") }
-        else
-          '{ println(s"(${${Expr(x.name)}}: ${${Expr(t.show)}}) => (${${Expr(body.show)}}): ${${Expr(u.show)}}") }
+      case f @ '{ type $t; type $u; ($x: `$t`) => ($bodyFn: `$t` => `$u`)(`$x`) } =>
+        val arr = if f.unseal.tpe <:< typeOf[$t =>*: $u] then Expr("=>*") else Expr("=>")
+        Expr.open(bodyFn)((body, _) =>
+          '{ println(s"\\(${${Expr(x.name)}}: ${${Expr(t.show)}}) ${$arr} (${${Expr(body.show)}} : ${${Expr(u.show)}})") }
+        )
 
-      case '{ type $a; type $b; val $x = ($t: ![`$a`]); ($body: `$b`) } =>
-        '{ println(s"let !${${Expr(x.name)}} be (${${Expr(t.show)}}): ${${Expr(a.show)}} in (${${Expr(body.show)}}): ${${Expr(b.show)}}") }
+      case '{ type $a; val $x = ($t: ![`$a`]); ($bodyFn: ![`$a`] => ?)(`$x`) } =>
+        Expr.open(bodyFn) { (body, close) =>
+          quoted.util.Var(t) { tVar =>
+            close(body)(tVar.get) match
+            case '{ $body: $b } =>
+              '{ println(s"let !${${Expr(x.name)}} be (${${Expr(t.show)}} : ![${${Expr(a.show)}}]) in (${${Expr(body.show)}} : ${${Expr(b.show)}})") }
+          }
+        }
 
-      case e => '{ println(s"expr ${${Expr(e.show)}} is of type ${${Expr(summon[Type[T]].show)}}") }
+      case e => '{ println(s"expr ${${Expr(e.unseal.underlyingArgument.show)}} is of type ${${Expr(summon[Type[T]].show)}}") }
 
   inline def traverseTypes[T](expr: => T): Unit = ${ traverseTypesImpl('expr) }
