b5ca4b88649486b437dfe005330119d0a7d95a71
bishabosha <bishbashboshjt@gmail.com>
2020-02-09 23:30:10 +0000

    update to dotty 0.22.0-RC1

--- eec-core/src/main/scala/mesa/eec/package.scala
+++ eec-core/src/main/scala/mesa/eec/package.scala
@@ -3,7 +3,7 @@ package mesa.eec
 import Trees.{Tree, ErasedTree}, Tree._
 
 
-inline def (sc: => StringContext) eec (args: => Any*) <: ErasedTree = ${ Macros.eecImpl('sc, 'args) }
+inline def (inline sc: StringContext) eec (inline args: Any*) <: ErasedTree = ${ Macros.eecImpl('sc, 'args) }
 
 def effect[T](op: => T) : Tree[T] = App(Bang(), Lazy(() => op))
 def succeed[T](value: T): Tree[T] = Pure(value)
