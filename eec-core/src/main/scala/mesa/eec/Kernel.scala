1136dbcf5225023c23d1ea68cb2bcfaf7ba37e05
bishabosha <bishbashboshjt@gmail.com>
2020-02-15 00:47:12 +0100

    only evaluate binders with correct apply kind

--- eec-core/src/main/scala/mesa/eec/Kernel.scala
+++ eec-core/src/main/scala/mesa/eec/Kernel.scala
@@ -14,6 +14,7 @@ object Kernel:
 
   enum Continuation:
     case Apply(e: Environment, arg: ErasedTree)
+    case Evaluate(e: Environment, arg: ErasedTree)
     case Pi(e: Environment, i: Int)
     case Sigma(e: Environment, inl: Name, l: ErasedTree, inr: Name, r: ErasedTree)
     case Bind(e: Environment, x: Name, u: ErasedTree)
@@ -31,8 +32,6 @@ object Kernel:
   given (given m: Mirror.ProductOf[Closure], rec: => Show[m.MirroredElemTypes]): Show[Closure] =
     Tuple.fromProductTyped(_).show
 
-  private def start[T](t: Tree[T]): State = (t, Nil, Nil)
-
   private def typeError(msg: String): Nothing = throw TypeError(msg)
 
   private extension on (s: State):
@@ -40,54 +39,61 @@ object Kernel:
     def stepPi: State = (s: @unchecked) match
     case ( Pair(x,_), _, Pi(e,0) :: ks ) => (x, e, ks)
     case ( Pair(_,y), _, Pi(e,1) :: ks ) => (y, e, ks)
-    case ( err      , _, Pi(_,i) :: _  ) => typeError(s"expected (_,_) as argument to ${List("fst","snd")(i)} but got: ${err.show}")
+    case ( err      , _, _             ) => typeError(s"expected (_,_) but got: ${err.show}")
 
     def stepSigma: State = (s: @unchecked) match
     case ( Inl(inl), _, Sigma(e,x,l,_,_) :: ks ) => (l, bind(e, x -> inl), ks)
     case ( Inr(inr), _, Sigma(e,_,_,y,r) :: ks ) => (r, bind(e, y -> inr), ks)
-    case ( err     , _, Sigma(_,_,_,_,_) :: _  ) => typeError(s"expected either (inl _) or (inr _) but got: ${err.show}")
+    case ( err     , _, _                      ) => typeError(s"expected either (inl _) or (inr _) but got: ${err.show}")
 
     def stepBind: State = (s: @unchecked) match
     case ( Bang(value), e, Bind(f,x,u) :: ks ) => (value, e                       , Reduce(f,x,u) :: ks)
     case ( l @ Lazy(_), e, Bind(f,x,u) :: ks ) => (u    , bind(e, x -> compute(l)), ks                 )
-    case ( err        , _, Bind(_,_,_) :: _  ) => typeError(s"expected (!_) but got: ${err.show}")
+    case ( err        , _, _                 ) => typeError(s"expected (!_) but got: ${err.show}")
 
     def stepReduce: State = (s: @unchecked) match
     case ( result, _, Reduce(e,x,u) :: ks ) => (u, bind(e, x -> result), ks)
 
     def stepUnZip: State = (s: @unchecked) match
     case ( Tensor(value, next), e, UnZip(f,x,y,u) :: ks ) => (value, e, ReduceLeft(f,x,y,next,u) :: ks)
-    case ( err                , _, UnZip(_,_,_,_) :: _  ) => typeError(s"expected (!_*:_) but got: ${err.show}")
+    case ( err                , _, _                    ) => typeError(s"expected (!_*:_) but got: ${err.show}")
 
     def stepReduceLeft: State = (s: @unchecked) match
     case ( result, _, ReduceLeft(e,x,y,next,u) :: ks ) => (u, bind(e, x -> result, y -> next) , ks)
 
     def stepApply: State = (s: @unchecked) match
-    case ( Lambda(x,t), e, Apply(f,m) :: ks ) => (t, Closure(x,m,f)::e, ks )
-    case ( err        , _, Apply(_,m) :: ks ) => throw TypeError(s"Tried to apply non-function ${err.show} to ${m.show}")
+    case ( Lam(x,t), e, Apply(f,m) :: ks ) => (t, Closure(x,m,f)::e, ks )
+    case ( err        , _, _             ) => typeError(s"expected (\\_._) but got ${err.show}")
+
+    def stepEvaluate: State = (s: @unchecked) match
+    case ( Lin(x,t), e, Evaluate(f,m) :: ks ) => (t, Closure(x,m,f)::e, ks )
+    case ( err        , _, _                ) => typeError(s"expected (^\\_._) but got ${err.show}")
 
     def stepRoot: State = (s: @unchecked) match
     case ( v @ Var(x)         , Closure(y,n,f)::e, ks ) => if x == y then (n,f,ks) else (v,e,ks)
-    case ( Application(n,m)   , e                , ks ) => (n, e, Apply(e,m)       :: ks)
+    case ( App(n,m)           , e                , ks ) => (n, e, Apply(e,m)       :: ks)
+    case ( Eval(n,m)          , e                , ks ) => (n, e, Evaluate(e,m)    :: ks)
     case ( Project(p,i)       , e                , ks ) => (p, e, Pi(e,i)          :: ks)
     case ( CaseExpr(p,x,l,y,r), e                , ks ) => (p, e, Sigma(e,x,l,y,r) :: ks)
     case ( Let(x,t,u)         , e                , ks ) => (t, e, Bind(e,x,u)      :: ks)
     case ( LetT(x,y,t,u)      , e                , ks ) => (t, e, UnZip(e,x,y,u)   :: ks)
     end stepRoot
 
-  @tailrec private def run(s: State): ErasedTree =
-    println(s"step: ${Show.show(s)}")
-    if s.finalState then s.continuation match
-      case (_: Pi)         :: _ => run(s.stepPi)
-      case (_: Apply)      :: _ => run(s.stepApply)
-      case (_: Sigma)      :: _ => run(s.stepSigma)
-      case (_: Bind)       :: _ => run(s.stepBind)
-      case (_: Reduce)     :: _ => run(s.stepReduce)
-      case (_: UnZip)      :: _ => run(s.stepUnZip)
-      case (_: ReduceLeft) :: _ => run(s.stepReduceLeft)
-      case Nil                  => s.head
+  @tailrec
+  private def run(state: State, debug: Boolean): ErasedTree =
+    if debug then println(s"step: ${Show.show(state)}")
+    if state.finalState then state.continuation match
+      case (_: Pi)         :: _ => run(state.stepPi, debug)
+      case (_: Apply)      :: _ => run(state.stepApply, debug)
+      case (_: Evaluate)   :: _ => run(state.stepEvaluate, debug)
+      case (_: Sigma)      :: _ => run(state.stepSigma, debug)
+      case (_: Bind)       :: _ => run(state.stepBind, debug)
+      case (_: Reduce)     :: _ => run(state.stepReduce, debug)
+      case (_: UnZip)      :: _ => run(state.stepUnZip, debug)
+      case (_: ReduceLeft) :: _ => run(state.stepReduceLeft, debug)
+      case Nil                  => state.head
     else
-      run(s.stepRoot)
+      run(state.stepRoot, debug)
   end run
 
   private def bind(env: Environment, bindings: (Name, ErasedTree)*): Environment =
@@ -98,51 +104,34 @@ object Kernel:
     case Fst(p) => (p, 0)
     case Snd(p) => (p, 1)
 
-  object Opaque:
+  object Opaque: // has no dependency
     def unapply(t: Pure[?] | Lazy[?] | Splice[?] | Point.type): true = true
 
-  object Effect:
+  object Effect: // effects require an enclosing expression to evaluate
     def unapply(t: Inl[?,?] | Inr[?,?] | Bang[?] | WhyNot[?]): true = true
 
-  object Prod:
+  object Prod: // products require an enclosing expression to project
     def unapply(t: Pair[?,?] | Tensor[?,?]): true = true
 
-  object Binder:
+  object Binder: // binder requires enclosing application to reduce
     def unapply(t: Lin[?,?] | Lam[?,?]): true = true
-
-  object Lambda:
-    def unapply(t: Lin[?,?] | Lam[?,?]): (Name, ErasedTree) = Tuple.fromProduct(t).asInstanceOf
-
-  object Application:
-    def unapply(t: ErasedTree): Option[(ErasedTree, ErasedTree)] = t match
-    case t: App[t,u]  => Some(Tuple.fromProductTyped(t))
-    case t: Eval[t,u] => Some(Tuple.fromProductTyped(t))
-    case _            => None
-    end unapply
 
   object Closed:
     def unapply(t: ErasedTree): Boolean = t match
-    case (
-      Opaque() // has no dependency
-    | Effect() // effects require an enclosing expression to evaluate
-    | Prod()   // products require an enclosing expression to project
-    | Binder() // binder requires enclosing application to reduce
-    ) => true
-    case _ => false
-    end unapply
+    case (Opaque() | Effect() | Prod() | Binder()) => true
+    case _                                         => false
 
   private def (s: State).finalState = s match
   case (Closed(), _  , _  ) => true
   case (Var(_)  , Nil, _  ) => true
   case _                    => false
-  end finalState
 
-  def reduce[T](t: Tree[T]): Either[TypeError, Tree[T]] =
-    try Right(run(start(t)).as)
+  def reduce[T](t: Tree[T], debug: Boolean = false): Either[TypeError, Tree[T]] =
+    try Right(run(state = (t, Nil, Nil), debug).as)
     catch case e: TypeError => Left(e)
 
-  def eval[T](t: Tree[T]): Either[TypeError, T] =
-    reduce(t).flatMap {
+  def eval[T](t: Tree[T], debug: Boolean = false): Either[TypeError, T] =
+    reduce(t, debug).flatMap {
       case Pure(t) => Right(t)
       case term    => Left(TypeError(s"Program terminated with ${term.show}"))
     }
