2b4ded34a4ccdb8779e4db5978e110e435b4aabc
bishabosha <bishbashboshjt@gmail.com>
2020-02-15 02:27:54 +0100

    collapse state transitions

--- eec-core/src/main/scala/mesa/eec/Kernel.scala
+++ eec-core/src/main/scala/mesa/eec/Kernel.scala
@@ -36,95 +36,70 @@ object Kernel:
 
   private extension on (s: State):
 
-    def stepPi: State = (s: @unchecked) match
-    case ( Pair(x,_), _, Pi(e,0) :: ks ) => (x, e, ks)
-    case ( Pair(_,y), _, Pi(e,1) :: ks ) => (y, e, ks)
-    case ( err      , _, _             ) => typeError(s"expected (_,_) but got: ${err.show}")
+    def betaReduce: State = (s: @unchecked) match
+      case ( Pair(x,_),   _, Pi(e,0)               :: ks ) => ( x, e, ks )
+      case ( Pair(_,y),   _, Pi(e,1)               :: ks ) => ( y, e, ks )
+      case ( Inl(t),      _, Sigma(e,l,u,_,_)      :: ks ) => ( u, bind(e,l->t), ks )
+      case ( Inr(t),      _, Sigma(e,_,_,r,u)      :: ks ) => ( u, bind(e,r->t), ks )
+      case ( t,           _, Reduce(e,x,u)         :: ks ) => ( u, bind(e,x->t), ks )
+      case ( t,           _, ReduceLeft(e,x,y,s,u) :: ks ) => ( u, bind(e,x->t,y->s), ks )
+      case ( t @ Lazy(_), _, Bind(e,x,u)           :: ks ) => ( u, bind(e,x->t.compute), ks )
+      case ( Bang(t),     e, Bind(f,x,u)           :: ks ) => ( t, e, Reduce(f,x,u)::ks )
+      case ( Tensor(t,s), e, UnZip(f,x,y,u)        :: ks ) => ( t, e, ReduceLeft(f,x,y,s,u)::ks )
+      case ( Lam(x,t),    e, Apply(f,m)            :: ks ) => ( t, Closure(x,m,f)::e, ks )
+      case ( Lin(x,t),    e, Evaluate(f,m)         :: ks ) => ( t, Closure(x,m,f)::e, ks )
+      case ( err,         _, (_: Pi)               :: ks ) => typeError(s"expected (_,_) but got: ${err.show}")
+      case ( err,         _, (_: Sigma)            :: ks ) => typeError(s"expected (inl _) or (inr _) but got: ${err.show}")
+      case ( err,         _, (_: Bind)             :: ks ) => typeError(s"expected (!_) but got: ${err.show}")
+      case ( err,         _, (_: UnZip)            :: ks ) => typeError(s"expected (!_*:_) but got: ${err.show}")
+      case ( err,         _, (_: Apply)            :: ks ) => typeError(s"expected (\\_._) but got ${err.show}")
+      case ( err,         _, (_: Evaluate)         :: ks ) => typeError(s"expected (^\\_._) but got ${err.show}")
 
-    def stepSigma: State = (s: @unchecked) match
-    case ( Inl(inl), _, Sigma(e,x,l,_,_) :: ks ) => (l, bind(e, x -> inl), ks)
-    case ( Inr(inr), _, Sigma(e,_,_,y,r) :: ks ) => (r, bind(e, y -> inr), ks)
-    case ( err     , _, _                      ) => typeError(s"expected either (inl _) or (inr _) but got: ${err.show}")
-
-    def stepBind: State = (s: @unchecked) match
-    case ( Bang(value), e, Bind(f,x,u) :: ks ) => (value, e                       , Reduce(f,x,u) :: ks)
-    case ( l @ Lazy(_), e, Bind(f,x,u) :: ks ) => (u    , bind(e, x -> compute(l)), ks                 )
-    case ( err        , _, _                 ) => typeError(s"expected (!_) but got: ${err.show}")
-
-    def stepReduce: State = (s: @unchecked) match
-    case ( result, _, Reduce(e,x,u) :: ks ) => (u, bind(e, x -> result), ks)
-
-    def stepUnZip: State = (s: @unchecked) match
-    case ( Tensor(value, next), e, UnZip(f,x,y,u) :: ks ) => (value, e, ReduceLeft(f,x,y,next,u) :: ks)
-    case ( err                , _, _                    ) => typeError(s"expected (!_*:_) but got: ${err.show}")
-
-    def stepReduceLeft: State = (s: @unchecked) match
-    case ( result, _, ReduceLeft(e,x,y,next,u) :: ks ) => (u, bind(e, x -> result, y -> next) , ks)
-
-    def stepApply: State = (s: @unchecked) match
-    case ( Lam(x,t), e, Apply(f,m) :: ks ) => (t, Closure(x,m,f)::e, ks )
-    case ( err        , _, _             ) => typeError(s"expected (\\_._) but got ${err.show}")
-
-    def stepEvaluate: State = (s: @unchecked) match
-    case ( Lin(x,t), e, Evaluate(f,m) :: ks ) => (t, Closure(x,m,f)::e, ks )
-    case ( err        , _, _                ) => typeError(s"expected (^\\_._) but got ${err.show}")
-
-    def stepRoot: State = (s: @unchecked) match
-    case ( v @ Var(x)         , Closure(y,n,f)::e, ks ) => if x == y then (n,f,ks) else (v,e,ks)
-    case ( App(n,m)           , e                , ks ) => (n, e, Apply(e,m)       :: ks)
-    case ( Eval(n,m)          , e                , ks ) => (n, e, Evaluate(e,m)    :: ks)
-    case ( Project(p,i)       , e                , ks ) => (p, e, Pi(e,i)          :: ks)
-    case ( CaseExpr(p,x,l,y,r), e                , ks ) => (p, e, Sigma(e,x,l,y,r) :: ks)
-    case ( Let(x,t,u)         , e                , ks ) => (t, e, Bind(e,x,u)      :: ks)
-    case ( LetT(x,y,t,u)      , e                , ks ) => (t, e, UnZip(e,x,y,u)   :: ks)
-    end stepRoot
+    def step: State = (s: @unchecked) match
+      case ( v @ Var(x),          Closure(y,n,f)::e, ks ) => if x == y then (n,f,ks) else (v,e,ks)
+      case ( App(t,m),            e,                 ks ) => (t, e, Apply(e,m)       :: ks)
+      case ( Eval(t,m),           e,                 ks ) => (t, e, Evaluate(e,m)    :: ks)
+      case ( Fst(t),              e,                 ks ) => (t, e, Pi(e,0)          :: ks)
+      case ( Snd(t),              e,                 ks ) => (t, e, Pi(e,1)          :: ks)
+      case ( CaseExpr(t,l,u,r,v), e,                 ks ) => (t, e, Sigma(e,l,u,r,v) :: ks)
+      case ( Let(x,t,u),          e,                 ks ) => (t, e, Bind(e,x,u)      :: ks)
+      case ( LetT(x,y,t,u),       e,                 ks ) => (t, e, UnZip(e,x,y,u)   :: ks)
 
   @tailrec
   private def run(state: State, debug: Boolean): ErasedTree =
-    if debug then println(s"step: ${Show.show(state)}")
-    if state.finalState then state.continuation match
-      case (_: Pi)         :: _ => run(state.stepPi, debug)
-      case (_: Apply)      :: _ => run(state.stepApply, debug)
-      case (_: Evaluate)   :: _ => run(state.stepEvaluate, debug)
-      case (_: Sigma)      :: _ => run(state.stepSigma, debug)
-      case (_: Bind)       :: _ => run(state.stepBind, debug)
-      case (_: Reduce)     :: _ => run(state.stepReduce, debug)
-      case (_: UnZip)      :: _ => run(state.stepUnZip, debug)
-      case (_: ReduceLeft) :: _ => run(state.stepReduceLeft, debug)
-      case Nil                  => state.head
+    if debug then
+      println(s"step: ${Show.show(state)}")
+    if state.redexOrFinal then
+      if state.continuation.isEmpty then
+        state.head
+      else
+        run(state.betaReduce, debug)
     else
-      run(state.stepRoot, debug)
+      run(state.step, debug)
   end run
 
   private def bind(env: Environment, bindings: (Name, ErasedTree)*): Environment =
     bindings.foldRight(env)((p, env) => if p.head == "_" then env else Closure(p(0), p(1), Nil)::env)
 
-  object Project:
-    def unapply(t: Fst[?,?] | Snd[?,?]): (ErasedTree, Int) = t match
-    case Fst(p) => (p, 0)
-    case Snd(p) => (p, 1)
+  object Redex:
+    def unapply(redex:
+        Pure[?]
+      | Splice[?]
+      | Lam[?,?]
+      | Lin[?,?]
+      | Inl[?,?]
+      | Inr[?,?]
+      | Bang[?]
+      | Lazy[?]
+      | WhyNot[?]
+      | Pair[?,?]
+      | Tensor[?,?]
+      | Point.type): true = true
 
-  object Opaque: // has no dependency
-    def unapply(t: Pure[?] | Lazy[?] | Splice[?] | Point.type): true = true
-
-  object Effect: // effects require an enclosing expression to evaluate
-    def unapply(t: Inl[?,?] | Inr[?,?] | Bang[?] | WhyNot[?]): true = true
-
-  object Prod: // products require an enclosing expression to project
-    def unapply(t: Pair[?,?] | Tensor[?,?]): true = true
-
-  object Binder: // binder requires enclosing application to reduce
-    def unapply(t: Lin[?,?] | Lam[?,?]): true = true
-
-  object Closed:
-    def unapply(t: ErasedTree): Boolean = t match
-    case (Opaque() | Effect() | Prod() | Binder()) => true
-    case _                                         => false
-
-  private def (s: State).finalState = s match
-  case (Closed(), _  , _  ) => true
-  case (Var(_)  , Nil, _  ) => true
-  case _                    => false
+  private def (s: State).redexOrFinal = s match
+  case (Redex(), _  , _ ) => true
+  case (Var(_) , Nil, _ ) => true
+  case _                  => false
 
   def reduce[T](t: Tree[T], debug: Boolean = false): Either[TypeError, Tree[T]] =
     try Right(run(state = (t, Nil, Nil), debug).as)
--- eec-core/src/main/scala/mesa/eec/package.scala
+++ eec-core/src/main/scala/mesa/eec/package.scala
@@ -5,6 +5,6 @@ import Trees.{Tree, ErasedTree}, Tree._
 
 inline def (inline sc: StringContext) eec (inline args: Any*) <: ErasedTree = ${ Macros.eecImpl('sc, 'args) }
 
-def compute[T](t: Lazy[T]): Tree[T] = succeed(t.op())
+def [T](t: Lazy[T]).compute: Tree[T] = succeed(t.op())
 def effect[T](op: => T): Tree[T]    = Lazy(() => op)
 def succeed[T](value: T): Tree[T]   = Pure(value)
